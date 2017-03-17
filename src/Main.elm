73f5fd3a6750045217738993ab00559dcb4587e5
Tessa Kelly <tessakkelly@gmail.com>
2017-03-17 16:41:48 -0700

    Condense and add base text inputs

--- src/Main.elm
+++ src/Main.elm
@@ -4,36 +4,87 @@ module Main exposing (main)
 @docs main
 -}
 
-import Html
-import Json.Decode exposing (Value, decodeString)
-import Flags exposing (decoder)
-import Model exposing (Model)
-import Update exposing (update)
-import View exposing (view)
+import Html exposing (..)
+import Html.A11y exposing (..)
+import Html.CssHelpers
+import Styles exposing (..)
+
+
+{- VIEW -}
+
+
+{-| view
+-}
+view : Model -> Html Msg
+view model =
+    div
+        [ class [ Container ] ]
+        [ Html.CssHelpers.style css
+        , viewInputs
+        ]
+
+
+viewInputs : Html msg
+viewInputs =
+    let
+        inputty typeAndValue =
+            { label = text "Label"
+            , typeAndValue = typeAndValue
+            , attributes = []
+            }
+    in
+        div []
+            [ leftLabeledInput <| inputty (textInput "Tessa")
+            , rightLabeledInput <| inputty (textInput "Tessa")
+            , br [] []
+            , leftLabeledInput <| inputty (radioInput "GroupName" "Radio 1" False)
+            , rightLabeledInput <| inputty (radioInput "GroupName" "Radio 2" True)
+            , br [] []
+            , leftLabeledInput <| inputty (checkboxInput "Checkbox 1" (Just False))
+            , leftLabeledInput <| inputty (checkboxInput "Checkbox 1" Nothing)
+            , rightLabeledInput <| inputty (checkboxInput "Checkbox 2" (Just True))
+            , invisibleLabeledInput (inputty (textInput "Is this hidden???")) "input-id"
+            ]
+
+
+
+{- MODEL -}
+
+
+type alias Model =
+    {}
+
+
+model : Model
+model =
+    {}
+
+
+
+{- UPDATE -}
+
+
+{-| Msg
+-}
+type Msg
+    = NoOp
+
+
+{-| update
+-}
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
 
 
 {-| main
 -}
-main : Program String Model Update.Msg
+main : Program Never {} Msg
 main =
-    Html.programWithFlags
-        { init = init
+    Html.beginnerProgram
+        { model = model
         , update = update
-        , subscriptions = subscriptions
         , view = view
         }
-
-
-init : String -> ( Model, Cmd msg )
-init pageData =
-    case decodeString decoder pageData of
-        Ok flags ->
-            Model.init flags ! []
-
-        Err err ->
-            Debug.crash err
-
-
-subscriptions : a -> Sub b
-subscriptions =
-    always Sub.none
