5f773a79b0bb9da7fa7f0db828ab531eecfe2e41
Tessa Kelly <tessakkelly@gmail.com>
2017-04-02 11:22:26 -0700

    Pull input views out into separate file

--- src/Main.elm
+++ src/Main.elm
@@ -5,9 +5,9 @@ module Main exposing (main)
 -}
 
 import Html exposing (..)
-import Html.A11y exposing (..)
 import Html.CssHelpers
 import Styles exposing (..)
+import Views.Inputs as Inputs
 
 
 {- VIEW -}
@@ -22,107 +22,8 @@ view model =
         [ Html.CssHelpers.style css
         , h1 [] [ text "Html.A11y View Examples" ]
         , h2 [] [ text "Inputs" ]
-        , viewInputs
+        , Inputs.view
         ]
-
-
-viewInputs : Html msg
-viewInputs =
-    div []
-        [ viewTextInputVariations
-        , viewRadioInputVariations
-        , viewCheckboxInputVariations
-        ]
-
-
-viewTextInputVariations : Html msg
-viewTextInputVariations =
-    let
-        inputModel =
-            { label = text "Text Label"
-            , typeAndValue = textInput "Value"
-            , attributes = []
-            }
-    in
-        viewInputType "Text"
-            { left = [ inputModel ]
-            , right = [ inputModel ]
-            , invisible = [ (,) inputModel ("input-id-text") ]
-            }
-
-
-viewRadioInputVariations : Html msg
-viewRadioInputVariations =
-    let
-        inputModel groupName =
-            { label = text "Radio Label"
-            , typeAndValue = radioInput groupName "Value" False
-            , attributes = []
-            }
-    in
-        viewInputType "Radio"
-            { left = [ inputModel "left", inputModel "left" ]
-            , right = [ inputModel "right", inputModel "right" ]
-            , invisible =
-                [ (,) (inputModel "invisible") ("input-id-radio-1")
-                , (,) (inputModel "invisible") ("input-id-radio-2")
-                ]
-            }
-
-
-viewCheckboxInputVariations : Html msg
-viewCheckboxInputVariations =
-    let
-        inputModel status =
-            { label = text <| "Checkbox Label, Status: " ++ toString status
-            , typeAndValue = checkboxInput "Value" status
-            , attributes = []
-            }
-    in
-        viewInputType "Checkbox"
-            { left = [ inputModel (Just True), inputModel Nothing, inputModel (Just False) ]
-            , right = [ inputModel (Just True), inputModel Nothing, inputModel (Just False) ]
-            , invisible =
-                [ (,) (inputModel (Just True)) ("input-id-checkbox-1")
-                , (,) (inputModel Nothing) ("input-id-checkbox-2")
-                , (,) (inputModel (Just False)) ("input-id-checkbox-3")
-                ]
-            }
-
-
-viewInputType :
-    String
-    -> { left : List (Input msg)
-       , right : List (Input msg)
-       , invisible : List ( Input msg, String )
-       }
-    -> Html msg
-viewInputType name { left, right, invisible } =
-    div []
-        [ h3 [] [ text <| name ++ " Inputs" ]
-        , leftLabeledInputHeader
-        , div [] (List.map leftLabeledInput left)
-        , rightLabeledInputHeader
-        , div [] (List.map rightLabeledInput right)
-        , invisibleLabeledInputHeader
-        , div [] (List.map (uncurry invisibleLabeledInput) invisible)
-        , br [] []
-        ]
-
-
-leftLabeledInputHeader : Html msg
-leftLabeledInputHeader =
-    h4 [] [ text "leftLabeledInput:" ]
-
-
-rightLabeledInputHeader : Html msg
-rightLabeledInputHeader =
-    h4 [] [ text "rightLabeledInput:" ]
-
-
-invisibleLabeledInputHeader : Html msg
-invisibleLabeledInputHeader =
-    h4 [] [ text "invisibleLabeledInput:" ]
 
 
 
